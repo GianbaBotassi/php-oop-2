c0d83210a8e1eae0de375bdd1475a34ecdc161ce
GianbaBotassi <gianba89@gmail.com>
2023-07-03 20:14:59 +0200

    Add basic layout to cycle objects

--- data/db.php
+++ data/db.php
@@ -7,88 +7,24 @@ require_once("./models/bed.php");
 require_once("./models/toy.php");
 
 
-// $products = [
-//   "dogs" => [
-//     "food" => [
-//       (object) [
-//         "name" => "crocsMaki",
-//         "price" => "20",
-//         "icon" => "iconaCane",
-//         "img" => "crocsDogs.jpg",
-//         "typology" => "food",
-//         "category" => "dogs"
-//       ]
-//     ],
-//     "toy" => [
-//       (object) [
-//         "name" => "toyMaki",
-//         "price" => "12",
-//         "icon" => "iconaCane",
-//         "img" => "toyDogs.jpg",
-//         "typology" => "toy",
-//         "category" => "dogs"
-//       ]
-//     ],
-//     "cuccia" => [
-//       (object) [
-//         "name" => "crocsMaki",
-//         "price" => "20",
-//         "icon" => "iconaCane",
-//         "img" => "cucciaDogs.jpg",
-//         "typology" => "cuccia",
-//         "category" => "dogs"
-//       ]
-//     ]
-//   ],
-//   "cats" => [
-//     "food" => [
-//       (object) [
-//         "name" => "crocsMaki",
-//         "price" => "20",
-//         "icon" => "iconaGatto",
-//         "img" => "crocsCats.jpg",
-//         "typology" => "food",
-//         "category" => "dogs"
-//       ]
-//     ],
-//     "toy" => [
-//       (object) [
-//         "name" => "crocsMaki",
-//         "price" => "20",
-//         "icon" => "iconaGatto",
-//         "img" => "toyCats.jpg",
-//         "typology" => "toy",
-//         "category" => "dogs"
-//       ]
-//     ],
-//     "cuccia" => [
-//       (object) [
-//         "name" => "crocsMaki",
-//         "price" => "20",
-//         "icon" => "iconaGatto",
-//         "img" => "cucciaCats.jpg",
-//         "typology" => "cuccia",
-//         "category" => "dogs"
-//       ]
-//     ]
-//   ]
-// ];
-
 $products = [];
 
 $dogsIcon = "iconaCane";
 $catsIcon = "iconaGatto";
 
 
-$dogsCategory = new Category("IconaCane");
-$catsCategory = new Category("IconaGatto");
+$dogsCategory = new Category("<i class='fa-solid fa-dog'></i>");
+$catsCategory = new Category("<i class='fa-solid fa-cat'></i>");
 
 
 
-$products[] = new Food("Crocchettine Arya", 99, "Immagine", $dogsCategory, "Cibo", 2022);
-$products[] = new Toy("Osso Maki", 10, "Immagine", $dogsCategory, "Giochi", "Palla");
-$products[] = new Bed("Cuccia Utah", 30, "Immagine", $catsCategory, "Cucce", "XL");
-// $products["dogs"]["food"][] = new Food("Crocchette Maki", 20, "Icona", "immagine", "Cibo", "Cani");
+$products["dogs"][] = new Food("Crocchettine", 22, "crocsDogs.jpg", $dogsCategory, "Cibo", 2022);
+$products["cats"][] = new Food("Carne in scatola", 33, "crocsCats.jpg", $catsCategory, "Cibo", 2025);
+$products["dogs"][] = new Toy("Kong", 10, "toyDogs.jpg", $dogsCategory, "Giochi", "Kong");
+$products["cats"][] = new Toy("Bastoncino", 5, "toyCats.jpg", $catsCategory, "Giochi", "Bastoncino");
+$products["dogs"][] = new Bed("Cuccia", 100, "cucciaDogs.jpg", $dogsCategory, "Cucce", "L");
+$products["cats"][] = new Bed("Cuccia", 80, "cucciaCats.jpg", $catsCategory, "Cucce", "XS");
+// $products["dogs"]["food"][] = new Food("Crocchette", 20, "Icona", "immagine", "Cibo", "Cani");
 
 
 // $prodottoNuovo = new Product("Crocchettine Arya", 99, "Immagine", $dogsCategory);
--- index.php
+++ index.php
@@ -4,11 +4,42 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <?php require_once("./data/db.php"); ?>
+  <link rel="stylesheet" href="style.css">
   <title>ZooBool</title>
 </head>
 
 <body>
+  <div class="container">
+    <h2>Prodotti Cani</h2>
+    <div class="row">
+      <?php
+      foreach ($products["dogs"] as $product) {
+        echo "<div class='slot'>
+                <h3>" . $product->getName() . "</h3>
+                <div><img src='./data/img/" . $product->getImg() . "'></div>
+                <div>Prezzo: " . $product->getPrice() . " €</div>
+                <div>Tipologia: " . $product->getItem() . "</div>
+              </div>";
+      }
+      ?>
+    </div>
+    <h2>Prodotti Gatti</h2>
+    <div class="row">
+      <?php
+      foreach ($products["cats"] as $product) {
+        echo "<div class='slot'>
+                <h3>" . $product->getName() . "</h3>
+                <div><img src='./data/img/" . $product->getImg() . "'></div>
+                <div>Prezzo: " . $product->getPrice() . " €</div>
+                <div>Tipologia: " . $product->getItem() . "</div>
+              </div>";
+      }
+      ?>
+    </div>
 
+  </div>
 </body>
 
 </html>
@@ -16,12 +47,10 @@
 <?php
 
 
-require_once("./data/db.php");
 
+// echo "<pre>", var_dump($products["dogs"]), "</pre>!";
 
-echo "<pre>", var_dump($products), "</pre>!";
+//  echo "
+// <pre>", var_dump($ciao), "</pre>!";
 
-
-
-
-// echo "<pre>", var_dump($ciao), "</pre>!";
+?>
