4d337be3eee4e97e627be47bfdb5f0c0bab2e3db
GianbaBotassi <gianba89@gmail.com>
2023-07-03 15:40:19 +0200

    Add basic database

---
+++ data/db.php
@@ -0,0 +1,65 @@
+<?php
+
+$products = [
+  "dogs" => [
+    "food" => [
+      "crocsMaki" => [
+        "name" => "crocsMaki",
+        "description" => "van bene per tutto",
+        "price" => 20
+      ],
+      "crocsArya" => [
+        "name" => "crocsArya",
+        "description" => "solo cani grossa taglia",
+        "price" => 50
+      ]
+    ],
+    "toy" => [
+      "giocoMaki" => [
+        "name" => "giocoMaki",
+        "description" => "ossicino",
+        "price" => 5
+      ],
+      "giocoArya" => [
+        "name" => "giocoArya",
+        "description" => "Osso GIGANTEEEEE",
+        "price" => 10
+      ]
+    ],
+    "cuccia" => [
+      "cucciaMaki" => [
+        "name" => "cucciaMaki",
+        "description" => "cuccia minuscola",
+        "price" => 35
+      ],
+      "cucciaArya" => [
+        "name" => "cucciaArya",
+        "description" => "cuccia come una casa",
+        "price" => 70
+      ]
+    ]
+  ],
+  "cats" => [
+    "food" => [
+      "croccheUtah" => [
+        "name" => "croccheUtah",
+        "description" => "mini crocchine gatto",
+        "price" => 15
+      ]
+    ],
+    "toy" => [
+      "giocoUtah" => [
+        "name" => "giocoUtah",
+        "description" => "tiragraffi",
+        "price" => 66
+      ]
+    ],
+    "cuccia" => [
+      "cucciaUtah" => [
+        "name" => "cucciaUtah",
+        "description" => "cuccia sul divano e letto",
+        "price" => 22
+      ]
+    ]
+  ]
+];
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+
+require_once("./data/db.php");
+
+
+echo "<pre>", var_dump($products["dogs"]["food"]), "</pre>!";
